1151572d112758a70a557a4fdee45b63f2685a05
asepjayamutakin12@gmail.com <rivaldwirexy@gmail.com>
2022-06-19 14:38:06 +0700

    firstCommitTugas

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,19 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
+import "./Contact.css";
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+const Contact = ({ data }) => {
+  return (
+    <section>
+      <img src={data.photo ? data.photo : ""} alt="" />
+      <div className="detail-contact">
+        <p className="name-contact">{data.name ? data.name : ""}</p>
+        <p>{data.phone ? data.phone : ""}</p>
+        <p>{data.email ? data.email : ""}</p>
+      </div>
+    </section>
+  );
+};
 
 export default Contact;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
-import React from 'react';
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <></>
-    )
-}
+  return (
+    <div>
+      <h1>Call a Friend</h1>
+      <p className="subtitle">your friendly contact app</p>
+      <p className="limit">
+        ................................................................................
+      </p>
+    </div>
+  );
+};
 
 export default Header;
